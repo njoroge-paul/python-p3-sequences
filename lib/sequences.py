5a0ea3109ec2b17271c75bd6ffb1134c7bdb540a
Duncan <njorogedun870@gmail.com>
2024-09-09 11:54:58 +0300

    all tests passed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+
+    fib_sequence=[]
+    a, b = 0, 1
+    for _ in range(length):
+        fib_sequence.append(a)
+        a, b = b, a+b
+    print(fib_sequence)
+    return fib_sequence
+
+print_fibonacci(5)
